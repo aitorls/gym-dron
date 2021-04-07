fb946fd285aff56b79929ca1911e6d5d59dd7aa7
aitorls <aitorcoy97@gmail.com>
2021-04-07 11:11:35 +0200

    hi

--- gym_dron/__init__.py
+++ gym_dron/__init__.py
@@ -1,18 +1,18 @@
 from gym.envs.registration import register
 
 register(
-    id='dron2dron_3D_C-v0',
+    id='dron2dron_3DC-v0',
     entry_point='gym_dron.envs:Dron2dron_3DC',
 )
 register(
-    id='dron2dron_2D_C-v0',
+    id='dron2dron_2DC-v0',
     entry_point='gym_dron.envs:Dron2dron_2DC',
 )
 register(
-    id='dron2dron_3D_D-v0',
+    id='dron2dron_3DD-v0',
     entry_point='gym_dron.envs:Dron2dron_3DD',
 )
 register(
-    id='dron2dron_2D_D-v0',
+    id='dron2dron_2DD-v0',
     entry_point='gym_dron.envs:Dron2dron_2DD',
 )
